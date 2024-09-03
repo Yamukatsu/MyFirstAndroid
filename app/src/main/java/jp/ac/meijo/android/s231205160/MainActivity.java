c50aef166a175e309057507f597164c17d250279
Yamukatsu <231205160@ccmailg.meijo-u.ac.jp>
2024-09-03 12:17:07 +0900

    add image

--- app/src/main/java/jp/ac/meijo/android/s231205160/MainActivity.java
+++ app/src/main/java/jp/ac/meijo/android/s231205160/MainActivity.java
@@ -4,21 +4,33 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijo.android.s231205160.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.text.setText(R.string.name);
+        var continue = ContextCompat.getDrawable(this, R.drawable.baseline_cached_24);
+        binding.imageView2.setImageDrawable(continue);
     }
 }
