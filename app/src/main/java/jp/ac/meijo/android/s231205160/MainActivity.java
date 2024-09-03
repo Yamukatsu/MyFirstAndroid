fb12812b87b979acfe856c14fdb81ea08e6a8846
Yamukatsu <231205160@ccmailg.meijo-u.ac.jp>
2024-09-03 15:40:39 +0900

    buttonとeditTextとDataStoreの実装

--- app/src/main/java/jp/ac/meijo/android/s231205160/MainActivity.java
+++ app/src/main/java/jp/ac/meijo/android/s231205160/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.meijo.android.s231205160;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,7 @@ import jp.ac.meijo.android.s231205160.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefDataStore = PrefDataStore.getInstance(this);
+
+        prefDataStore.getString("name")
+                        .ifPresent(name -> binding.text.setText(name));
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -29,8 +37,48 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        binding.text.setText(R.string.name);
-        var continue = ContextCompat.getDrawable(this, R.drawable.baseline_cached_24);
-        binding.imageView2.setImageDrawable(continue);
+        binding.text.setText(R.string.text);
+
+        binding.button2.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
+
+        binding.button3.setOnClickListener(view -> {
+            binding.text.setText("");
+            binding.editTextText.setText("");
+            prefDataStore.delete("name");
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        prefDataStore.getString("name")
+                .ifPresent(name -> binding.text.setText(name));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        var text = binding.editTextText.getText().toString();
+        prefDataStore.setString("name", text);
     }
 }
